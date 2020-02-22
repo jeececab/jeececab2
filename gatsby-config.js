72f4b2d92b6d21721b74e3654b4dc7d13b2c05d4
jeececab <jcseguincabana@gmail.com>
2020-02-22 09:53:17 -0500

    testing perf stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,15 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
     {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        useMozJpeg: false,
+        stripMetadata: true,
+        defaultQuality: 75,
+      },
+    },
+    "gatsby-transformer-sharp",
+    {
       resolve: "gatsby-plugin-i18n",
       options: {
         langKeyDefault: "en",
