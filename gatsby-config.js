e097a4dc367ee32fd54ad2913317e3270ec58e78
jeececab <jcseguincabana@gmail.com>
2020-02-22 08:46:06 -0500

    performance fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,10 +10,30 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: "src/images/favicon.png",
+        name: "Jeececab - Front-end developer",
+        short_name: "Jeececab",
+        description:
+          "Personal website of Jean-Christophe Séguin Cabana (aka Jeececab), front-end developer based in Montreal.",
+        lang: "en",
+        display: "standalone",
+        icon: `src/images/favicon.png`,
+        start_url: "/",
+        background_color: "#080808",
+        theme_color: `#00bb92`,
+        localize: [
+          {
+            start_url: "/fr/",
+            lang: "fr",
+            name: "Jeececab - Développeur Front-end",
+            short_name: "Jeececab",
+            description:
+              "Site Web personnel de Jean-Christophe Séguin Cabana (aka Jeececab), développeur front-end basé à Montréal.",
+          },
+        ],
       },
     },
+    "gatsby-plugin-offline",
   ],
 }
